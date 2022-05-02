409721ab1eeb27337979f7f1d7021361ef0b142a
istrav <travis.burandt@gmail.com>
2022-05-02 15:19:07 -0500

    port 5

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
-let port = process.env.PORT || 8080
+let port = process.env.PORT || 4000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
