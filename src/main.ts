d0e3bbdbe35f8bd2b23367e48cabde2bd3c18c43
istrav <travis.burandt@gmail.com>
2022-05-02 15:14:13 -0500

    port 4

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
-let port = process.env.PORT || 1337
+let port = process.env.PORT || 8080
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors();
   app.useWebSocketAdapter(new WsAdapter(app));
 
-  await app.listen(3000);
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
